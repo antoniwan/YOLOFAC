3d04dbae42d74c8b249114879224e3af90aa57fe
Antonio Rodríguez <antoniwan@gmail.com>
2014-02-22 14:08:49 -0500

    Sendgrid setup for local development is now in place.

---
+++ app/config/local/mail.php
@@ -0,0 +1,12 @@
+<?php
+
+return array(
+	'driver' => 'smtp',
+	'host' => 'smtp.sendgrid.net',
+	'port' => 587,
+	'from' => array('address' => 'system@yoloforacause.com', 'name' => '#YOLO for a Cause'),
+	'encryption' => 'tls',
+	'username' => 'yolofac',
+	'password' => 'wtfover1231',
+	'pretent' => true
+);
