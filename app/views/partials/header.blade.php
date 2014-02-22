7e40a51a7d644b09e7d270eb9b24bfac21b7ff1d
Waldemar Figueroa <waldemar@nobox.com>
2014-02-22 15:22:15 -0500

    Pretty social buttons

--- app/views/partials/header.blade.php
+++ app/views/partials/header.blade.php
@@ -1,20 +1,22 @@
-<nav class="top-bar" data-topbar>
-  <ul class="title-area">
-    <li class="name">
-      <h1><a href="{{ route('home') }}">#YOLO for a cause</a></h1>
-    </li>
-    <li class="toggle-topbar menu-icon"><a href="#">Menu</a></li>
-  </ul>
-
-  <section class="top-bar-section">
-    <!-- Right Nav Section -->
-    <ul class="right">
-      <li><a class="button" href="#">Create a dare</a></li>
+<div class="contain-to-grid">
+  <header class="top-bar" data-topbar>
+    <ul class="title-area">
+      <li class="name">
+        <h1><a href="{{ route('home') }}">#YOLO for a cause</a></h1>
+      </li>
+      <li class="toggle-topbar menu-icon"><a href="#">Menu</a></li>
     </ul>
 
-    <!-- Left Nav Section -->
-    <ul class="left">
-      <li><a href="#">Browser dares</a></li>
-    </ul>
-  </section>
-</nav>
+    <section class="top-bar-section">
+      <!-- Right Nav Section -->
+      <ul class="right">
+        <li><a class="button" href="#">Create a dare</a></li>
+      </ul>
+
+      <!-- Left Nav Section -->
+      <ul class="left">
+        <li><a href="#">Browse dares</a></li>
+      </ul>
+    </section>
+  </header>
+</div>
