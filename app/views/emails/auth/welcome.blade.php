836dd8001a6a568f7d9be0a5e786ed7e36e08858
Antonio Rodríguez <antoniwan@gmail.com>
2014-02-22 14:34:24 -0500

    Welcome email is now ready for some real styling. Copy is FPO but makes sense.

--- app/views/emails/auth/welcome.blade.php
+++ app/views/emails/auth/welcome.blade.php
@@ -5,5 +5,13 @@
 @stop
 
 @section('content')
-
+    <h2>Hey {{'[name]'}}, thanks for joining #YOLOFAC</h2>
+    <strong>What now?</strong>
+    <ul>
+        <li><a href="#">Dare your friends</a> into doing something you've ever wanted</li>
+        <li>Complete your <a href="#">profile</a></li>
+        <li>Check out other <a href="#">dares</a></li>
+        <li>Check out <a href="#">how much</a> our user-base have contributed to charities</li>
+        <li><a href="#">Share</a> the great news and make your friends become part of this community for good</li>
+    </ul>
 @stop
--- app/views/layouts/email.blade.php
+++ app/views/layouts/email.blade.php
@@ -17,9 +17,19 @@
 
 
     </head>
-    <body data-base="{{ url() }}" data-assets="{{ asset('') }}" data-route="{{ Route::current()->getName() }}">
+    <body class="html-email-template" data-base="{{ url() }}" data-assets="{{ asset('') }}" data-route="{{ Route::current()->getName() }}">
+        <div class="container">
+            <header>
+                <h1>#YOLO for a Cause</h1>
+            </header>
 
-        @yield('content')
+            <div id="content">
+                @yield('content')
+            </div>
 
+            <footer>
+                <p>You're receiving this because you registered to our application. Unsubscribe <a href="#">here</a>.</p>
+                <p>Battlehack Miami 2014 | Team OneBearDown | Doing it live since 1980.</p>
+            </footer>
     </body>
 </html>
