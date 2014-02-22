66e4ac021471b77f41717c3f592d52a00d35bf4c
Antonio Rodríguez <antoniwan@gmail.com>
2014-02-22 17:47:28 -0500

    Index init.

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -5,5 +5,32 @@
 @stop
 
 @section('content')
-    <h1>Hello World!</h1>
+    <div id="homepage-hero">
+    	<div class="overlay"></div>
+    	<img src="{{ asset('img/index/hero.gif') }}" alt="challengers unite!">
+    	<div class="row">
+    		<div class="large-12 columns">
+    			<h2>We dare you, to do some good!</h2>
+    		</div>
+    	</div>
+	</div>
+
+	<div class="row">
+		<div class="large-12 columns">
+			<p>We dare you to dare your friends for the profit of Charities and the entertainment of the intertubes.</p>
+		</div>
+	</div>
+	<div class="row">
+		<div class="large-6 columns">
+			Featured Charities
+		</div>
+		<div class="large-6 columns">
+			Latest Dares
+		</div>
+	</div>
+	<div class="row">
+		<div class="large-12 columns">
+			<h3>Check out your impact!</h3>
+		</div>
+	</div>
 @stop
