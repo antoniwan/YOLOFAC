4d71a35b4f5c0126e82b8e3b856252211c43cd47
Waldemar Figueroa <waldemar@nobox.com>
2014-02-22 14:53:39 -0500

    Further work on Register page.

--- app/views/register.blade.php
+++ app/views/register.blade.php
@@ -1,14 +1,32 @@
 @extends('layouts.master');
 
 @section('content')
-    <main class="row">
-        <div class="small-12 column">
-            <h1>Register</h1>
-            <ul class="list-unstyled">
-                <li><a class="button btn-social--facebook" href="{{$service_urls['facebook']}}">Facebook</a></li>
-                <li><a class="button btn-social--twitter" href="#">Twitter</a></li>
-                <li><a class="button btn-social--google" href="#">Google+</a></li>
-            </ul>
+    <main class="page row">
+        <div class="row">
+            <div class="medium-7 large-8 column">
+                <header>
+                    <h1 class="page__title">Sign in to create a #YOLO dare</h1>
+                    <p>Register with any of your social networks to start creating your #YOLO Dare and start daring your friends.</p>
+                </header>
+
+                <ul class="social-btn-group list-unstyled small-block-grid-2">
+                    <li>
+                        <a class="button btn-social--facebook" href="{{ $service_urls['facebook'] }}">Facebook</a>
+                    </li>
+                    <li>
+                        <a class="button btn-social--twitter" href="#">Twitter</a>
+                    </li>
+                    <li>
+                        <a class="button btn-social--google" href="#">Google</a>
+                    </li>
+                </ul>
+                <p>We won't share your information with any third-party or send you any spam.</p>
+            </div>
+
+            <div class="medium-5 large-4 column">
+                <p class="lead">People already do crazy stuff and share them on the internet, why not back them up for good causes!</p>
+                <p>Dare your friends to do something and commit to donate to a charity for each completed dare.</p>
+            </div>
         </div>
     </main>
 @show
