f6756d14fcf6ad6809109e75e35cfdedd7fe904d
Antonio Rodríguez <antoniwan@gmail.com>
2014-02-23 00:33:08 -0500

    I dare you email template init.

---
+++ app/views/emails/idareyou.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.email')
+
+@section('title')
+    <title>I DARE YOU! | #YOLO for a Cause</title>
+@stop
+
+@section('content')
+    <h2>{{'[name]'}} has dared you!</h2>
+    <p>Find out what's going on <a href="#">here</a>.</p>
+@stop
