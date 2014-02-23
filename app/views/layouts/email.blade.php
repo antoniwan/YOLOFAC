3f449557a6a430c5bfd677ef8954192ebb95af2d
Antonio Rodríguez <antoniwan@gmail.com>
2014-02-23 02:13:12 -0500

    Fucking email html is still not working. Fuck this shit.

--- app/views/layouts/email.blade.php
+++ app/views/layouts/email.blade.php
@@ -1,18 +1,13 @@
 <!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9" lang="en"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<html>
     <head>
         <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
         @section('title')
         <title>?</title>
         @show
 
-        @section('stylesheets')
-            <style type="text/css">
+        <style type="text/css">
             * {
                 padding: 0;
                 margin: 0;
@@ -60,8 +55,7 @@
             a {
                 color: #37485D;
             }
-            </style>
-        @show
+        </style>
 
 
     </head>
